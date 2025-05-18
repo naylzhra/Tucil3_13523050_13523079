243cf636406bfcd1dee3bd46dd2c7c31655b004f
Mayla Yaffa <maylayaffa12@gmail.com>
2025-05-18 12:07:40 +0700

    feat: node

---
+++ src/Node.java
@@ -0,0 +1,22 @@
+public class Node implements Comparable<Node> {
+    public Board board;
+    public Node parent;
+    public String move;    
+    public int g;          // g(n)
+    public int h;          // h(n)
+    public int f;          // g(n) + h(n)
+
+    public Node(Board board, Node parent, String move, int g, int h) {
+        this.board = board;
+        this.parent = parent;
+        this.move = move;
+        this.g = g;
+        this.h = h;
+        this.f = g + h;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return Integer.compare(this.f, other.f);
+    }
+}
