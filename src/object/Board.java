bd61ae6338b4f88b27897e5d4f1516e89359a354
Mayla Yaffa <maylayaffa12@gmail.com>
2025-05-19 08:21:53 +0700

    feat: move functions in board

--- src/object/Board.java
+++ src/object/Board.java
@@ -1,4 +1,6 @@
 package object;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Board {
@@ -48,6 +50,58 @@ public class Board {
         for (Piece p : pieces) {
             p.print();
         }
+    }
+
+    public Board applyMove(Piece p, String dir) {
+        char[][] newGrid = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            newGrid[i] = Arrays.copyOf(grid[i], width);
+        }
+
+        List<Piece> newPieces = new ArrayList<>();
+        for (Piece piece : pieces) {
+            if (piece.id == p.id) {
+                Piece moved = piece.move(dir);  
+                newPieces.add(moved);
+
+                for (int i = 0; i < piece.length; i++) {
+                    if (piece.isHorizontal) {
+                        newGrid[piece.row][piece.col + i] = '.';
+                    } else {
+                        newGrid[piece.row + i][piece.col] = '.';
+                    }
+                }
+
+                for (int i = 0; i < moved.length; i++) {
+                    if (moved.isHorizontal) {
+                        newGrid[moved.row][moved.col + i] = moved.id;
+                    } else {
+                        newGrid[moved.row + i][moved.col] = moved.id;
+                    }
+                }
+            } else {
+                newPieces.add(new Piece(piece.id, piece.row, piece.col, piece.length, piece.isHorizontal, piece.isPrimary));
+            }
+        }
+
+        return new Board(newGrid, newPieces, goalRow, goalCol, exitDir);
+    }
+
+    public boolean canMove(Piece p, String dir) {
+        if (p.isHorizontal) {
+            if (dir.equals("L")) {
+                if (p.col - 1 >= 0 && grid[p.row][p.col - 1] == '.') return true;
+            } else if (dir.equals("R")) {
+                if (p.col + p.length < width && grid[p.row][ p.col + p.length] == '.') return true;
+            }
+        } else { 
+            if (dir.equals("U")) {
+                if (p.row - 1 >= 0 && grid[p.row - 1][p.col] == '.') return true;
+            } else if (dir.equals("D")) {
+                if (p.row + p.length < height && grid[p.row + p.length][p.col] == '.') return true;
+            }
+        }
+        return false;
     }
 
 
